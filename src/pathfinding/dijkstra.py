9e1e95765107315f654ebd182596b532a5b5bb0e
Gabriel Guia Machado <gguia-ma@c1r5s9.42lisboa.com>
2026-07-06 19:04:53 +0100

    yens a

---
+++ src/pathfinding/dijkstra.py
@@ -0,0 +1,62 @@
+
+import heapq
+from typing import Dict, List, Optional, Tuple
+from ..models import Graph, Zone, ZoneType
+
+
+class PathNotFoundError(Exception):
+    def __init__(self, start: Zone, end: Zone) -> None:
+        self.start = start
+        self.end = end
+        super().__init__(f"No path found from '{start.name}' to '{end.name}'")
+
+
+class Dijkstra:
+    @staticmethod
+    def find_path(graph: Graph, start: Zone, end: Zone) -> List[Zone]:
+        _, previous = Dijkstra._compute_distances(graph, start, end)
+
+        return Dijkstra._reconstruct_path(previous, start, end)
+    
+
+    @staticmethod
+    def _compute_distances(graph: Graph, start: Zone, end: Zone) -> Tuple[Dict[Zone, int], Dict[Zone, Optional[Zone]]]:
+        distances: Dict[Zone, int] = {start: 0}
+        previous: Dict[Zone, Optional[Zone]] = {}
+        counter = 0
+        queue: List[Tuple[int, str, int, Zone]] = []
+        heapq.heappush(queue, (0, start.name, counter, start))
+
+        while queue:
+            actual_distance, _, _, actual_zone = heapq.heappop(queue)
+
+            if actual_zone.is_end:
+                break
+            if actual_distance > distances.get(actual_zone, float('inf')):
+                continue
+            for neighbour in graph.get_neighbors(actual_zone):
+                if neighbour.zone_type == ZoneType.BLOCKED:
+                    continue
+                cost = neighbour.zone_type.movement_cost()
+                new_distance = actual_distance + cost
+                if new_distance < distances.get(neighbour, float('inf')):
+                    distances[neighbour] = new_distance
+                    previous[neighbour] = actual_zone
+                    counter += 1
+                    heapq.heappush(queue, (new_distance, neighbour.name, counter, neighbour))
+
+        return distances, previous
+    
+
+    @staticmethod
+    def _reconstruct_path(previous: Dict[Zone, Optional[Zone]], start: Zone, end: Zone) -> List[Zone]:
+        if end not in previous and end == start:
+            raise PathNotFoundError(start, end)
+        path = [end]
+        actual = end
+        while actual != start:
+            actual = previous[actual]
+            path.append(actual)
+
+        path.reverse()
+        return path
---
+++ src/pathfinding/k_paths.py
@@ -0,0 +1,48 @@
+import copy
+from typing import List
+from ..models import Graph, Zone, ZoneType
+from .dijkstra import Dijkstra, PathNotFoundError
+
+
+class KPaths:
+    @staticmethod
+    def find_k_paths(graph: Graph, start: Zone, end: Zone, k: int) -> List[List[Zone]]:
+        A: List[List[Zone]] = []   # acepted paths
+        B: List[List[Zone]] = []   # candidates to chose
+
+        A.append(Dijkstra.find_path(graph, start, end))
+
+        while len(A) < k:
+            last_path = A[-1]
+            for i, spur_node in enumerate(last_path[:-1]):
+                root_path = last_path[0 : i + 1]
+                graph_copy = copy.deepcopy(graph)
+                for path in A:
+                    if path[0:i+1] == root_path and len(path) > i+1:
+                        zones_to_block = path[i+1]
+                        zone_in_copy = graph_copy.get_zone(zone.name)
+                        zone_in_copy.zone_type = ZoneType.BLOCKED
+                for node in root_path[:-1]:
+                    if node == spur_node:
+                        continue
+                    zone = graph_copy.get_zone(node.name)
+                    zone.zone_type = ZoneType.BLOCKED
+                try:
+                    spur_node_copy = graph_copy.get_zone(spur_node.name)
+                    spur_path = Dijkstra.find_path(graph_copy, spur_node_copy, end)
+
+                    candidate = root_path[:-1] + spur_path
+                    if candidate not in A and candidate not in B:
+                        B.append(candidate)
+                except PathNotFoundError:
+                    continue
+
+            if not B:
+                break
+            B.sort(key=lambda path: KPaths._path_cost(path))
+            best = B.pop(0)
+            A.append(best)
+        
+        return A
+
+
